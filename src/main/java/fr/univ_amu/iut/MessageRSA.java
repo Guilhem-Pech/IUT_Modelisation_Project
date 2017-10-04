fcd58649aa40bdd6faae87f88f632fcef9993e08
p16001500 <guilhempech@gmail.com>
2017-10-04 16:06:20 +0200

    Broken

--- src/main/java/fr/univ_amu/iut/MessageRSA.java
+++ src/main/java/fr/univ_amu/iut/MessageRSA.java
@@ -1,15 +1,19 @@
 package fr.univ_amu.iut;
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Math.random;
+
 
 /**
  * Created by p16001500 on 28/09/17.
  */
 
 public class MessageRSA {
-    private ArrayList cryptedmessage;
+    private ArrayList<Integer> cryptedmessage;
+    private String decryptedMessage;
     private int n;
     private int e;
     private int d;
@@ -17,13 +21,27 @@ public class MessageRSA {
     private int q;
 
 
-    public MessageRSA(int msg) {
-        this.generateKey();
+    public MessageRSA() {
+        cryptedmessage = new ArrayList<Integer>();
+        decryptedMessage = "";
     }
 
-    public MessageRSA(char msg) {
-        this((int)msg);
+    public MessageRSA(String message){
+        this();
+        decryptedMessage = message;
+        cryptedmessage = new ArrayList<Integer>();
+        this.generateKey();
+        this.cryptString();
     }
+
+
+
+    public MessageRSA(ArrayList<Integer> message){
+        this();
+        cryptedmessage = message;
+        decryptedMessage = "";
+    }
+
 
     private static int euclide(int b, int n){
         int v = 0;
@@ -50,31 +68,96 @@ public class MessageRSA {
     }
 
 
-    private static int pgcd(int a, int b) {
-        while (a%b != 0) {
-            a = b;
-            b = a % b;
-        }
-        return b;
+    private static int pgcd(int a,int b) {
+        int r = a;
+        while (r!=0)  {r = a%b; a=b; b=r; }
+        return(Math.abs(a));
     }
 
+    private static boolean isFirst(int n)
+    {
+        if(n<=1) return false;
+        for(int i = 2;i*i<=n;i++)
+        {
+            if (n%i ==0)
+                return false;
+            i++;
+        }
+        return true;
+    }
     private static int random_between(int j,int k){
         return (int) (random()*(k-j+1)+j);
     }
 
-    private static int randFirstNumber(int a){
-        int r = random_between(2,a);
+    private static int randFirstNumber(int min,int a){
+        int r = random_between(min,a);
         while (pgcd(a,r) != 1)
-         r = random_between(2,a);
+         r = random_between(min,a);
         return r;
     }
 
-    public void generateKey(){
+    private void generateKey(){
+        p = 1009;
+        q = 1999;
+
+
         n = p * q;
-        int phi = (p - 1) * (q-1); 
-        d = randFirstNumber(phi);
+        int phi = (p - 1) * (q-1);
         e = euclide(d,phi);
+        d = randFirstNumber((p<q?q:p),phi);
+
     }
 
+    private int cryptInt(int i){
+        return (i^e)%n;
+    }
 
+    private int cryptChar(char k){
+        return cryptInt((int)k);
+    }
+
+    private void cryptString(){
+        cryptedmessage.clear();
+        for (char c: decryptedMessage.toCharArray()) {
+            cryptedmessage.add(cryptChar(c));
+        }
+    }
+
+    private char decryptChar(int i){
+        System.out.println(i);
+        return (char) ((i^d)%n);
+    }
+
+    private int decryptInt(int i){
+        return ((i^d)%n);
+    }
+
+    private void decryptString(){
+        for (int c: cryptedmessage ) {
+            decryptedMessage.concat(String.valueOf(decryptChar(c)));
+        }
+    }
+
+    public int[] getPublicKeys(){
+        int[] publicKey = {n,e};
+        return publicKey;
+    }
+
+    public int[] getPrivateKeys() {
+        int[] publicKey = {n, d};
+        return publicKey;
+    }
+    public void setPrivateKey(int n, int d){
+        this.n = n;
+        this.d = d;
+    }
+
+    public ArrayList getCryptedMessage(){
+        return cryptedmessage;
+    }
+
+    public String getUncryptedMessage(){
+        this.decryptString();
+        return decryptedMessage;
+    }
 }
